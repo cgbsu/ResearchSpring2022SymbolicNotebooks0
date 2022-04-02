a885ec4045615bc69c8a847ea22a26805599ce54
cgbsu@github.com <cgreeley@student.bridgew.edu>
2022-04-02 03:23:33 +0000

    Did a bunch of work on the stepper, added a bunch of stuff that I think will make it more useful!

---
+++ work/custom_libraries/.ipynb_checkpoints/schrodinger-checkpoint.py
@@ -0,0 +1,90 @@
+import sympy as sp
+import sympy.physics.units.quantities as sq
+from sympy.physics.quantum.constants import hbar
+
+PSI_FUNCTION = sp.Function( "psi" )
+POTENTIAL_FUNCTION = sp.Function( "V" )
+TOTAL_ENERGY_SYMBOL = sq.Symbol( 'E', nonzero = True, positive = True )
+MASS_SYMBOL = sq.Quantity( 'm', positive = True, nonzero = True )
+POSITION_SYMBOL = sp.Symbol( 'x', positive = True )
+
+def time_independent_schroedinger_equation( 
+            psi = PSI_FUNCTION, 
+            potential = POTENTIAL_FUNCTION, 
+            total_energy = TOTAL_ENERGY_SYMBOL, 
+            mass = MASS_SYMBOL, 
+            reduced_planck_constant = hbar, #REDUCED_PLANCK_CONSTANT_SYMBOL, 
+            position = POSITION_SYMBOL 
+        ): 
+    return sp.Eq( 
+            ( ( -( reduced_planck_constant ** 2 ) / ( 2 * mass ) ) \
+                    * sp.Derivative( psi( position ), ( position, 2 ) ) )
+                    + ( potential( position ) * psi( position ) ), 
+            total_energy * psi( position ) 
+        )
+
+class ZeroPotential( sp.Function ): 
+    @classmethod
+    def eval( cls, position ):
+        return 0
+
+#https://docs.sympy.org/latest/modules/assumptions/index.html#querying
+#https://docs.sympy.org/latest/modules/codegen.html#module-sympy.codegen.cxxnodes
+
+class Potential( sp.Function ): 
+    DEFAULT_POTENTIAL = sp.Symbol( "V_0" )
+    @classmethod
+    def eval( cls, position, potential = DEFAULT_POTENTIAL ): 
+        return potential
+
+class TunnelPotential( sp.Function ): 
+    DEFAULT_WELL_LENGTH = sp.Symbol( 'L' )
+    DEFAULT_POTENTIAL = Potential.DEFAULT_POTENTIAL
+    DEFAULT_START = 0
+    @classmethod
+    def eval( cls, position, length = DEFAULT_WELL_LENGTH, 
+             start = DEFAULT_START, potential = DEFAULT_POTENTIAL ): 
+        if position < start or position > sp.simplify( length + start ): 
+            return ZeroPotential.eval( position )
+        return Potential.eval( position, potential )
+
+class StairWell( sp.Function ): 
+    UNIFORM_LENGTH_SYMBOL = sp.Symbol( 'L' )
+    UNIFORM_POTENTIAL_SYMBOL = sp.Symbol( 'V' )
+    UNIFORM_STAIR_LENGTHS = ( 
+            UNIFORM_LENGTH_SYMBOL, 
+            UNIFORM_LENGTH_SYMBOL, 
+            UNIFORM_LENGTH_SYMBOL 
+        )
+    UNIFORM_POTENTIALS = ( 
+            UNIFORM_POTENTIAL_SYMBOL, 
+            2 * UNIFORM_POTENTIAL_SYMBOL / 3, 
+            3 * UNIFORM_POTENTIAL_SYMBOL / 3
+        )
+    NON_UNIFORM_STAIR_LENGTHS = sp.symbols( "L_0 L_1 L_2" )
+    NON_UNIFORM_POTENTIALS = sp.symbols( "V_0 V_1 V_2" )
+    #NON_UNIFORM_ASSUMPTIONS = sp.Q.gt( NON_UNIFORM_POTENTIALS[ 0 ], NON_UNIFORM_POTENTIALS[ 1 ] ) \
+    #        & sp.Q.gt( NON_UNIFORM_POTENTIALS[ 1 ], NON_UNIFORM_POTENTIALS[ 2 ] ) \
+    #        & sp.Q.gt( NON_UNIFORM_STAIR_LENGTHS[ 0 ] + NON_UNIFORM_STAIR_LENGTHS[ 1 ], NON_UNIFORM_STAIR_LENGTHS[ 0 ] ) \
+    #        & sp.Q.gt( NON_UNIFORM_STAIR_LENGTHS[ 2 ] + NON_UNIFORM_STAIR_LENGTHS[ 1 ], NON_UNIFORM_STAIR_LENGTHS[ 2 ] )  
+    #DEFAULT_ASSUMPTIONS = NON_UNIFORM_ASSUMPTIONS \
+    #        & sp.Q.positive( UNIFORM_LENGTH_SYMBOL ) \
+    #        & sp.Q.positive( UNIFORM_POTENTIAL_SYMBOL )
+    DEFAULT_START = 0
+    
+    @classmethod
+    def eval( 
+                cls, 
+                position, 
+                start = DEFAULT_START, 
+                potentials = NON_UNIFORM_POTENTIALS, 
+                lengths = NON_UNIFORM_STAIR_LENGTHS#, 
+                #assumptions = DEFAULT_ASSUMPTIONS 
+            ): 
+        position = position + start
+        if position < lengths[ 0 ]: 
+            return potentials[ 0 ]
+        elif position < ( lengths[ 0 ] + lengths[ 1 ] ): 
+            return potentials[ 1 ]
+        elif position < ( lengths[ 0 ] + lengths[ 1 ] + lengths[ 2 ] ): 
+            return potentials[ 2 ]
---
+++ work/custom_libraries/stepper.py
@@ -0,0 +1,217 @@
+import sympy as sp
+import sympy.physics.units.quantities as sq
+from sympy.physics.quantum.constants import hbar
+
+set_equal = lambda to_set, value : sp.Eq( to_set, value )
+both_sides = lambda equation, operation : sp.Eq( operation( equation.lhs ), operation( equation.rhs ) )
+equation_to_dict = lambda equation : { equation.lhs : equation.rhs }
+
+class Stepper: 
+
+    LEFT = "LEFT", 
+    RIGHT = "RIGHT"
+    DEFAULT_APPLY = both_sides
+    CONSTANT_SUBSTITUTION = lambda step, constant : step.subs( equation_to_dict( constant ) )    
+    DEFAULT_GET_ELEMENT = lambda step, element : { 
+            Stepper.LEFT : lambda step : step.lhs, 
+            Stepper.RIGHT : lambda step : step.rhs, 
+            0 : lambda step : step.lhs, 
+            1 : lambda step : step.rhs, 
+        }[ element ]( step )
+    # Will be subscripted
+    DEFAULT_CONSTANT_NAME_BASE = 'K'
+    # Will be subscripted
+    DEFAULT_CHECKPOINT_NAME_BASE = "checkpoint"
+    
+    def __init__( self, 
+                first_step, new_steps = None, 
+                apply = DEFAULT_APPLY, 
+                constant_substitution = CONSTANT_SUBSTITUTION, 
+                get_element = DEFAULT_GET_ELEMENT, 
+                default_constant_name_base = DEFAULT_CONSTANT_NAME_BASE, 
+                default_checkpoint_name_base = DEFAULT_CHECKPOINT_NAME_BASE, 
+                default_assumptions = []
+            ): 
+        self.steps = new_steps if new_steps else []
+        self.steps.append( first_step )
+        self.chaining = False
+        self.apply = both_sides
+        self.constant_substitution = constant_substitution
+        self.get_element = get_element
+        self.check_points = {}
+        self.constants = {}
+        self.default_constant_name_base = default_constant_name_base
+        self.default_checkpoint_name_base = default_checkpoint_name_base
+        self.assumptions = tuple( default_assumptions )
+    
+    def step_number( self, step = None ): 
+        return ( len( self.steps ) - 1 ) if not step else step
+    
+    def last_step( self, step = None ):
+        return self.steps[ self.step_number( step ) ]
+    
+    def _return_chain( self, step, chain ): 
+        return self if ( chain or self.chaining ) else step        
+    
+    def add_step( self, new_step, chain = False ):
+        self.steps.append( new_step )
+        return self._return_chain( self.steps[ -1 ], chain )
+
+    def operate( self, operation, step = None, chain = False ):
+        self.steps.append( operation( self.last_step( step ) ) )
+        return self._return_chain( self.steps[ -1 ], chain )
+
+    def _apply_operation( self, apply ): 
+        return self.apply if not apply else apply
+    
+    def _constant_substitution_operation( self, constant_substiution ): 
+        return self.constant_substitution if not constant_substiution else constant_substiution
+
+    def _get_element_operation( self, get_element ): 
+        return self.get_element if not get_element else get_element
+    
+    def manipulate( self, operation, chain = False, apply = None ): 
+        return self._return_chain( self.add_step( 
+                self._apply_operation( apply )( self.last_step(), operation ) ), 
+                chain 
+            )
+    
+    def delete_step( self, step ): 
+        old_step = self.steps[ step ]
+        del self.steps[ step ]
+        return old_step
+    
+    def undo( self, step = None, chain = False ): 
+        return self._return_chain( 
+                self.delete_step( self.step_number( step ) ), 
+                chain 
+            )
+    
+    def clone( self, from_step = None ):
+        return self.branch( lambda blank : blank, from_step )
+    
+    def branch( self, operation = None, from_step = None, apply = None ): 
+        from_step = self.step_number( from_step )
+        return Stepper( 
+                self._apply_operation( apply )( self.last_step( from_step ), operation ), 
+                self.steps[ : self.step_number( from_step ) : ], 
+            )
+    
+    def substitute_constant( self, constant, chain = False, constant_substitute = None ):
+            return self.operate( lambda step : 
+                    self._constant_substitution_operation( constant_substitute )( step, constant ), 
+                    chain = chain 
+                )
+    
+    def symbols( self, last_step = None ): 
+        last_step = self.last_step( last_step )
+        return list( last_step.atoms() ) + list( last_step.atoms( sp.Function ) )
+    
+    def rename( self, old_symbol, new_symbol_name, chain = False ): 
+        last_step = self.last_step()
+        assert old_symbol in last_step.atoms() or old_symbol in last_step.atoms( sp.Function )
+        return self.add_step( last_step.subs( 
+            old_symbol if type( old_symbol ) == dict \
+                    else { old_symbol : new_symbol_name } ), chain 
+                )
+    
+    def chain( self, operation ):
+        self.chaining = True
+        operation( self )
+        self.chaining = False
+        return self
+    
+    def begin_chain( self ): 
+        self.chaining = True
+        return self
+    
+    def end_chain( self ): 
+        self.chaining = False
+        return self
+    
+    def retrieve_element( self, element, from_step = None, get_element = None ): 
+        return self._get_element_operation( get_element )( self.last_step( from_step ), element )
+    
+    # Creature comforts
+    def left( self, from_step = None, get_element = None ): 
+        return self.retrieve_element( Stepper.LEFT, from_step, get_element )
+
+    def right( self, from_step = None, get_element = None ): 
+        return self.retrieve_element( Stepper.RIGHT, from_step, get_element )
+    
+    def _default_name( self, name, name_base, number ): 
+        return name_base + "_" + str( number ) if not name else name
+
+    def _default_constant_name( self, name ): 
+        return self._default_name( name, self.default_constant_name_base, len( self.constants ) )
+
+    def _default_check_point_name( self, name ): 
+        return self._default_name( name, self.default_checkpoint_name_base, len( self.check_points ) )
+    
+    def _default_name( self, name, name_base, number ): 
+        return name_base + "_" + str( number ) if not name else name
+
+    def add_assumption( self, assumption, chain = False ): 
+        self.assumptions = tuple( list( self.assumptions ) + [ assumption ] )
+        return self._return_chain( self.assumptions, chain )
+    
+    def element_to_constant( self, element, constant_name = None, from_step = None, chain = False, get_element = None ): 
+        constant = sp.Symbol( self._default_constant_name( constant_name ) )
+        self.constants[ constant ] = Stepper( sp.Eq( constant, self.retrieve_element( element, from_step, get_element ) ) )
+        return self._return_chain( self.constants[ constant ], chain )
+
+    def left_to_constant( self, constant_name = None, from_step = None, chain = False, get_element = None ): 
+        return self.element_to_constant( Stepper.LEFT, constant_name, from_step, chain, get_element )
+
+    def right_to_constant( self, constant_name = None, from_step = None, chain = False, get_element = None ): 
+        return self.element_to_constant( Stepper.RIGHT, constant_name, from_step, chain, get_element )
+    
+    def check_point( self, name_or_marker = None, from_step = None, chain = False ): 
+        checkpoint_name = self._default_check_point_name( name_or_marker )
+        self.check_points[ checkpoint_name ] = self.last_step( from_step )
+        return self._return_chain( self.check_points[ checkpoint_name ], chain )
+    
+    def constant_names( self ): 
+        return list( self.constants.keys() )
+    
+    def check_point_markers( self ): 
+        return list( self.check_points.keys() )
+    
+    # If you are wondering where I got the funny names from: 
+    # https://en.wikipedia.org/wiki/Multiplication
+    
+    def multipy( self, multiplicand, chain = False, apply = None ): 
+        return self.manipulate( lambda side : side * multiplicand, chain, apply )
+    
+    def divide( self, denomonator, chain = False, apply = None ): 
+        return self.manipulate( lambda side : side / denomonator, chain, apply )
+    
+    def add( self, addend, chain = False, apply = None ): 
+        return self.manipulate( lambda side : side + addend, chain, apply )
+    
+    def subtract( self, subtrahend, chain = False, apply = None ): 
+        return self.manipulate( lambda side : side - subtrahend, chain, apply )
+    
+    def raise_to_power( self, power, chain = False, apply = None ): 
+        return self.manipulate( lambda side : side ** power, chain, apply )
+    
+    def root( self, degree, chain = False, apply = None ): 
+        return self.manipulate( lambda side : sp.root( side, degree ), chain, apply )
+
+    def __mul__( self, multiplicand ): 
+        return self.manipulate( lambda side : side * multiplicand, True )
+    
+    def __truediv__( self, denomonator ): 
+        return self.manipulate( lambda side : side / denomonator, True )
+    
+    def __add__( self, addend ): 
+        return self.manipulate( lambda side : side + addend, True )
+    
+    def __sub__( self, subtrahend ): 
+        return self.manipulate( lambda side : side - subtrahend, True )
+    
+    def __pow__( self, power ): 
+        return self.manipulate( lambda side : side ** power, True )
+    
+    
+    
